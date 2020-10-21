9b35d2d9e9b8e8878b7a52aa122fc3f1b34afe68
Janie Kim <janiekim@Janies-MacBook-Pro.local>
2020-10-21 15:44:43 -0500

    complete app 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      things: ['Hydroflask', 'WiFi', 'Pillow']
+    };
+  }
+
+  render(){
+    let listOfThings = this.state.things.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Janie's List of Things</h1>
+      {listOfThings}
     </div>
-  );
+    )
+  }
 }
+  
 
 export default App;
