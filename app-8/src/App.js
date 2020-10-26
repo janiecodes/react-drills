2f5d09a4ed38d94e39c62a156df1da8148f34b94
Janie Kim <janiekim@Janies-MacBook-Pro.local>
2020-10-26 15:46:21 -0500

    saving point

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      blastoise = ""
+    } 
+  }
+
+  componentDidMount(){
+    axios.get('https://pokeapi.co/api/v2/pokemon/blastoise')
+    .then(response => {
+      this.setState({
+        blastoise: response.data
+      })
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+        <h1>Name: {this.state.blastoise.name}</h1>
+        <h1>Ability: {this.state.blastoise.ability}</h1>
+        <h1>Power: {this.state.blastoise.power}</h1>
+        <h1>Color: {this.state.blastoise.color}</h1>
+      </div>
+    )
+  }
+
 }
+
+
+
+
+
 
 export default App;
